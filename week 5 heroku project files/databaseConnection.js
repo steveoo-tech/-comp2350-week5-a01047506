a83c4badffeefb2cc3e1bd624a4aadcf26d9e1b1
steveoo-tech <60714891+steveoo-tech@users.noreply.github.com>
2021-02-16 21:33:08 -0800

    update data information

--- week 5 heroku project files/databaseConnection.js
+++ week 5 heroku project files/databaseConnection.js
@@ -5,9 +5,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 //mysql://b6fb24cb4c8c37:702be0f5@us-cdbr-east-03.cleardb.com/heroku_a79dfbc72e06757?reconnect=true
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b6fb24cb4c8c37",
-	password: "702be0f5",
-	database: "heroku_a79dfbc72e06757",
+	user: "b4660ac9cd5477",
+	password: "df4ab85d",
+	database: "heroku_598e4e58cf25c19",
 	multipleStatements: false
 };
 
